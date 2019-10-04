bc44dd104cb14a94b5e0350f95756de7aa023e84
ben4594 <ben4594@gmail.com>
2019-10-04 16:14:00 +0100

    Update guns-c.lua

--- guns-c.lua
+++ guns-c.lua
@@ -7,7 +7,7 @@ RegisterCommand("clear", function()
 end)
 RegisterCommand("givepistolandknife", function()
 	
-	ESX.TriggerServerCallback("esx_marker:fetchUserRank", function(playerRank)
+	ESX.TriggerServerCallback("EmergencyH:fetchUserRank", function(playerRank)
 		if playerRank == "admin" or playerRank == "superadmin" or playerRank == "mod" then
 			giveWeapon("weapon_pistol")
 			giveWeapon("weapon_knife")
@@ -35,7 +35,7 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1)
 		if IsControlJustReleased(1, h_key) then
-			ESX.TriggerServerCallback("esx_marker:fetchUserRank", function(playerRank)
+			ESX.TriggerServerCallback("EmergencyH:fetchUserRank", function(playerRank)
         		if playerRank == "admin" or playerRank == "superadmin" or playerRank == "mod" then
 					giveWeapon("weapon_pistol")
 					giveWeapon("weapon_knife")
@@ -47,7 +47,7 @@ Citizen.CreateThread(function()
 		end
 		if IsControlJustReleased(1, x_key) then
 			
-			ESX.TriggerServerCallback("esx_marker:fetchUserRank", function(playerRank)
+			ESX.TriggerServerCallback("EmergencyH:fetchUserRank", function(playerRank)
 				if playerRank == "admin" or playerRank == "superadmin" or playerRank == "mod" then
 					RemoveAllPedWeapons(GetPlayerPed(-1, true))
 					alert("~r~Cleared all weapons")
